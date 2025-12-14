64e778aa617e359357113acebb2ee5157c473298
suman2k3 <116755944+suman2k3@users.noreply.github.com>
2025-12-15 02:31:39 +0530

    Modify CORS settings for FastAPI
    Updated CORS configuration to include a new frontend URL and removed an old one.

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
 from database import engine
 import models
 from sweets import router as sweets_router
 from auth import router as auth_router
 
+# Create tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# =======================
+# CORS CONFIG (IMPORTANT)
+# =======================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "http://192.168.1.5:5173",
-                   "https://sweet-shop-management-system-5pvzbll8q-suman2k3s-projects.vercel.app"],
-        # frontend URL
-   
-
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sweet-shop-management-system-alpha.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
+# =======================
+# ROUTERS
+# =======================
 app.include_router(auth_router)
 app.include_router(sweets_router)
 
-
+# =======================
+# HEALTH CHECK
+# =======================
 @app.get("/")
 def home():
     return {"message": "Sweet Shop API running"}
-from fastapi import Depends
-from models import User
